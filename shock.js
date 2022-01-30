7e691ebea731776f278e708fd23e18e6e1af5fd5
Art Goldhammer <art.goldhammer@gmail.com>
2022-01-29 19:28:57 -0500

    tweaked formatting

--- shock.js
+++ shock.js
@@ -1,42 +1,63 @@
 const puppeteer = require('puppeteer');
 
+const SINCE_BIDEN = '#react-tabs-0';
+const PAST7 = '#react-tabs-2';
+const YEST = '#react-tabs-4';
+const LABEL_SELECTOR = 'ul.stats__list li.stats__stat div.stats__stat-name';
+const STATS_SELECTOR = 'ul.stats__list li.stats__stat div.stats__stat-number div';
 
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
 
-  const SINCE_BIDEN = '#react-tabs-0';
-  const PAST7 = '#react-tabs-2';
-  const YEST = '#react-tabs-4'
-  // const SEARCH_SELECTOR = 'input[placeholder=Search]';
-  const LABEL_SELECTOR = 'ul.stats__list li.stats__stat div.stats__stat-name';
-  // const LABELS_SELECTOR = 'ul.stats__list li.stats__stat ~ div.stats__stat-name';
-  const STATS_SELECTOR = 'ul.stats__list li.stats__stat div.stats__stat-number div';
-  // const RESULTS_SELECTOR = '.results-tab';
-
-  await page.goto('https://shockmarket.org/');
-  await page.click(SINCE_BIDEN);
+async function scrape_stats(which, page) {
+  await page.click(which);
   await page.waitForTimeout(500);
 
-  const labels = await page.$$(LABEL_SELECTOR);
-  // console.log('len labels', labels.length);
+  let labels = await page.$$(LABEL_SELECTOR);
   let decoded_labels = [];
   
   for (let i = 0; i < labels.length; i++) {
-      const label = await labels[i].evaluate(element => element.innerText);
+      let label = await labels[i].evaluate(element => element.innerText);
       // console.log('label', label);
       decoded_labels.push(label);
   }
 
   const stats = await page.$$(STATS_SELECTOR);
-  // console.log('len labels', stats.length);
+  let run_name = "";
+  switch (which) {
+      case SINCE_BIDEN:
+          run_name = "Since Biden's Election\n...\n"
+          break;
+      case PAST7:
+          run_name = "Past 7 Days\n...\n";
+          break;
+      case YEST:
+          run_name = "Yesterday\n...\n";
+          break;
+  }
+
+  console.log("\nRun: ", run_name);
   
   for (let i = 0; i < stats.length; i++) {
-      const stat = await stats[i].evaluate(element => element.innerText)
-      console.log('scraped result:', decoded_labels[i], stat);
+      let stat = await stats[i].evaluate(element => element.innerText)
+      console.log('scraped result:', decoded_labels[i], ":", stat);
   }
-      
-  // await page.screenshot({path: 'shocker.png'});
+
+  // return new Promise(() => which);
+
+};
+
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  
+  await page.goto('https://shockmarket.org/');
+
+  await scrape_stats(SINCE_BIDEN, page);
+  await scrape_stats(PAST7, page);
+  await scrape_stats(YEST, page);
+
+  await page.waitForTimeout(1000);
 
   await browser.close();
 })();
